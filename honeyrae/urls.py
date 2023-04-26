d0722addff17246e3257e9b8d203c90efe78a7f7
Mary Williams <sbyardnss@gmail.com>
2023-04-26 11:57:08 -0500

    customer GET requests created and working

--- honeyrae/urls.py
+++ honeyrae/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
-from repairsapi.views import register_user, login_user
+from repairsapi.views import register_user, login_user, CustomerView
+from rest_framework import routers
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'customers', CustomerView, 'customer')
 
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
